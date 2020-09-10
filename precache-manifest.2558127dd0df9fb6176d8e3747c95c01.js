91bd1bba50b82bc5c811287abafed7d1f2420450
Yussif <djiceman.gh@gmail.com>
2020-09-10 10:13:29 +0000

    to-do app build

---
+++ precache-manifest.2558127dd0df9fb6176d8e3747c95c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4124b95a8440a15477f9713a9d9bd8d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17ff1d10a496b6b5674c",
+    "url": "/static/css/main.3d399022.chunk.css"
+  },
+  {
+    "revision": "e6c5f9755b0c348d7302",
+    "url": "/static/js/2.78de81ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78de81ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ff1d10a496b6b5674c",
+    "url": "/static/js/main.5ccf8235.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
